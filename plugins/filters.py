32b21093a6de7139c552565505ed1bcfd3d82748
njanunni777 <85037210+njanunni777@users.noreply.github.com>
2021-06-05 23:19:03 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -168,7 +168,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             keyboard = InlineKeyboardMarkup([
                 [InlineKeyboardButton("HELP", callback_data="help_data"),
                     InlineKeyboardButton("ABOUT", callback_data="about_data")],
-                [InlineKeyboardButton("⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/cinemapranthanmaar")]
+                [InlineKeyboardButton("⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/pdxmoviesmp4")]
             ])
 
             await query.message.edit_text(
